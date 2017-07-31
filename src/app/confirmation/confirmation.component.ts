010f8cd6524200844cbfde1b0c6bbf8176a9836f
Louis Kraemer <louis.kraemer@its-future.com>
2017-07-31 09:32:06 +0200

    Complete confirmation js Added new tab opening in "hebergement"

--- src/app/confirmation/confirmation.component.ts
+++ src/app/confirmation/confirmation.component.ts
@@ -42,50 +42,11 @@ export class ConfirmationComponent implements OnInit {
   updateInfo() {
     this.userService.updateUser(this.updateUser, this.userKey).then(_ => {
       this.updateUser = null;
-      this.showConfirmPopup;
+      this.showConfirmPopup = true;
       setTimeout(() => {
         this.showConfirmPopup = false;
       }, 2000);
     })
   }
-
-    // getUsers() {
-    //     this.userService.getUsers()
-    //         .then(data => {
-    //         this.user = data;
-    //     });
-    // }
-    //
-    // updateInfo() {
-    //     this.userService.modifyUser(this.updateUser);
-    //     this.updateUser = null;
-    //     this.showConfirmPopup = true;
-    //     setTimeout(() => {
-    //         this.showConfirmPopup = false;
-    //     }, 2000);
-    // }
-    //
-    // search(value) {
-    //     this.searchUser = value.toLowerCase();
-    //     this.searchResult = [];
-    //     for (var i in this.user) {
-    //         const lastFirst = this.user[i].lastName.toLowerCase() + " " + this.user[i].firstName.toLowerCase();
-    //         const firstLast = this.user[i].firstName.toLowerCase() + " " + this.user[i].lastName.toLowerCase();
-    //         if ((lastFirst.indexOf(this.searchUser) != -1 || firstLast.indexOf(this.searchUser) != -1) && this.searchUser != "" && this.searchResult.length < 5) {
-    //             this.searchResult.push(this.user[i])
-    //         }
-    //     }
-    // }
-    //
-    // selectUser(event) {
-    //     for (var i in this.user) {
-    //         if (this.user[i]._id == event.target.id) {
-    //             this.searchResult = [];
-    //             this.searchUser = "";
-    //             this.updateUser = this.user[i];
-    //             break;
-    //         }
-    //     }
-    // }
 
 }
